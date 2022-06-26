37e0dcc7cb76ae5005386d3b6c68b46fb031a3c5
saumyagupta2025 <saumyagupta2025@gmail.com>
2022-06-26 20:03:15 +0530

    webapp.py

--- app.py
+++ app.py
@@ -416,7 +416,34 @@ def welcome():
 def __main__():
 
     #st.footer("Hello")
+    footer="""<style>
+    a:link , a:visited{
+    color: blue;
+    background-color: transparent;
+    text-decoration: underline;
+    }
 
+    a:hover,  a:active {
+    color: red;
+    background-color: transparent;
+    text-decoration: underline;
+    }
+
+    .footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: white;
+    color: black;
+    text-align: center;
+    }
+    </style>
+    <div class="footer">
+    <p>Developed with ❤ by <a style='display: block; text-align: center;' href="https://www.heflin.dev/" target="_blank">Heflin Stephen Raj S</a></p>
+    </div>
+    """
+    st.markdown(footer,unsafe_allow_html=True)
 
 
     hide_streamlit_style = """
