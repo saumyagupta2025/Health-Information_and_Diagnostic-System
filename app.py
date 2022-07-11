42a249f1846689c398c27ccf0372d85002191602
saumyagupta2025 <saumyagupta2025@gmail.com>
2022-07-11 17:13:44 +0530

    Project files

--- app.py
+++ app.py
@@ -415,8 +415,8 @@ def TbHome():
 def predict_tuberculosis():
     st.set_option('deprecation.showfileUploaderEncoding', False)
 
-    fp = "cnn.h5"
-    cnn = load_model(fp)
+    fp = "cnn.pkl"
+    cnn = pickle.load(fp)
     # @st.cache(allow_output_mutation=True)
     #cnn = loading_model()
 
